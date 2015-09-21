2905129cb2bd95251bf1effc3e0562ed9fb326fe
omarram <omarramadneh@gmail.com>
2015-09-21 13:53:15 +0300

    Added validations to fix broken models tests

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,6 +2,8 @@ class Author < ActiveRecord::Base
   has_many :publications
   has_many :books, through: :publications
 
+  validates_presence_of :first_name, :last_name
+
   def name
     "#{first_name} #{last_name}"
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
   has_many :publications
   has_many :authors, through: :publications
+
+  validates_presence_of :title
 end
