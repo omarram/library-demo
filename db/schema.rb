68be4cc534c99a39f69a4a43f38950e42cc8b8f5
omarram <omarramadneh@gmail.com>
2015-09-25 22:08:04 +0300

    Tiny fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,52 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150925100158) do
-
-  create_table "authors", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "biography"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "books", force: :cascade do |t|
-    t.string   "title"
-    t.string   "isbn"
-    t.text     "description"
-    t.string   "publisher"
-    t.date     "published_on"
-    t.string   "cover_url"
-    t.boolean  "on_loan",      default: false
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-  end
-
-  create_table "borrowers", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "loaned_books", force: :cascade do |t|
-    t.integer  "book_id"
-    t.integer  "borrower_id"
-    t.datetime "due_date_time"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  add_index "loaned_books", ["book_id", "borrower_id"], name: "index_loaned_books_on_book_id_and_borrower_id"
-
-  create_table "publications", force: :cascade do |t|
-    t.integer  "book_id"
-    t.integer  "author_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "publications", ["author_id"], name: "index_publications_on_author_id"
-  add_index "publications", ["book_id"], name: "index_publications_on_book_id"
+ActiveRecord::Schema.define(version: 0) do
 
 end
